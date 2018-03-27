76c86f2996d3c13c99b07bf92333269686889c09
Alciona <Alciona@users.noreply.github.com>
2018-03-27 12:36:22 +0300

    some mistake for learning

--- Calculate.java
+++ Calculate.java
@@ -3,7 +3,17 @@ public class Calculate{
 		System.out.println("Calculate...");
 		int first = Integer.valueOf(arg[0]);
 		int second = Integer.valueOf(arg[1]);
-		int summ = first + second;
-		System.out.println("Summ = " + summ);
+		char mathop =  arg[2].charAt(0);
+		int dec = 43;
+		char asc = (char) dec;
+		System.out.println("ASCII; " + asc);
+		System.out.println("first = " + first);
+		System.out.println("second = " + second);
+		System.out.println("mathop2 = " + arg[2]);
+		System.out.println("mathop = " + mathop);
+		
+		
+		int res = (int) first + (char) asc + (int) second;
+		System.out.println("Res = " + res);
 	}
 }
